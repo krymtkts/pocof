734e9237ea85b8f5cc9a5753eed1813f0229dab2
krymtkts <takatoshi.kuriyama@gmail.com>
2022-06-11 18:15:07 +0900

    Implement a case-sensitive option for each query.

--- src/pocof/Query.fs
+++ src/pocof/Query.fs
@@ -5,25 +5,57 @@ open System.Management.Automation
 open System.Text.RegularExpressions
 
 module PocofQuery =
-    let internal (=%=) wcp o =
-        WildcardPattern
-            .Get(wcp, WildcardOptions.IgnoreCase) // TODO: refer ther case_sensitive option.
-            .IsMatch(o)
-
-    let internal (=~=) regex o = Regex.IsMatch(o, regex)
-
     let run (s: PocofData.InternalState) (l: PSObject list) =
+        let (==) r l =
+            let opt =
+                if s.Filter.CaseSensitive then
+                    StringComparison.CurrentCulture
+                else
+                    StringComparison.CurrentCultureIgnoreCase
+
+            if r = String.Empty then
+                r.Equals(l, opt)
+            else
+                true
+
+        let (=*=) wcp o =
+            let opt =
+                if s.Filter.CaseSensitive then
+                    WildcardOptions.None
+                else
+                    WildcardOptions.IgnoreCase
+
+            WildcardPattern.Get(wcp, opt).IsMatch(o)
+
+        let (=~=) regex o =
+            let op =
+                if s.Filter.CaseSensitive then
+                    RegexOptions.None
+                else
+                    RegexOptions.IgnoreCase
+
+            let r =
+                try
+                    new Regex(regex, op)
+                with
+                | _ -> new Regex(String.Empty, op)
+
+            r.IsMatch(o)
+
         let value o =
             if s.Filter.CaseSensitive then
-                o.ToString()
+                o.ToString() // TODO: refer the property if specified.
             else
                 o.ToString().ToLower()
 
         let is =
-            match s.Filter.Matcher with
-            | PocofData.EQ -> ((=) s.Query)
-            | PocofData.LIKE -> ((=%=) s.Query)
-            | PocofData.MATCH -> ((=~=) s.Query) // TODO: check the invlaid pattern.
+            let op =
+                match s.Filter.Matcher with
+                | PocofData.EQ -> (==)
+                | PocofData.LIKE -> (=*=)
+                | PocofData.MATCH -> (=~=)
+
+            op s.Query
 
         let answer a = if s.Filter.Invert then not a else a
 
