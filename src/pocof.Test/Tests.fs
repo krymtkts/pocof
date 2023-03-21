479fd7f842011813b662bc5e8676351fbe28bb17
krymtkts <takatoshi.kuriyama@gmail.com>
2023-03-21 16:18:08 +0900

    Add tests for PocofQuery.props.

--- src/pocof.Test/Tests.fs
+++ src/pocof.Test/Tests.fs
@@ -642,5 +642,38 @@ module PocofData =
             [<Fact>]
             let ``should return any difference when a tab is entered.`` () = noop PocofData.TabExpansion
 
+module PocofQuery =
+    let state: PocofData.InternalState =
+        { Query = ""
+          QueryState =
+            { Matcher = PocofData.Matcher.MATCH
+              Operator = PocofData.Operator.OR
+              CaseSensitive = false
+              Invert = false }
+          PropertySearch = PocofData.PropertySearch.NonSearch
+          Notification = ""
+          SuppressProperties = false }
+
+    module run =
+        ()
+
+    module props =
+        let entries = [ "Name"; "Attribute"; "Length" ]
+
+        [<Fact>]
+        let ``should returns OK with emtpy list.`` () =
+            PocofQuery.props { state with PropertySearch = PocofData.PropertySearch.NonSearch } entries
+            |> shouldEqual (Ok [])
+
+        [<Fact>]
+        let ``should returns Error with 'Property not found'.`` () =
+            PocofQuery.props { state with PropertySearch = PocofData.PropertySearch.Search "No" } entries
+            |> shouldEqual (Error "Property not found")
+
+        [<Fact>]
+        let ``should returns Ok with filtered properties.`` () =
+            PocofQuery.props { state with PropertySearch = PocofData.PropertySearch.Search "Na" } entries
+            |> shouldEqual (Ok [ "Name" ])
+
 [<EntryPoint>]
 let main argv = 0
