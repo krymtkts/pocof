6185cde1e152f4ff99de812debaa4cd546677061
krymtkts <takatoshi.kuriyama@gmail.com>
2025-01-12 17:17:27 +0900

    Add benchmarks for `Handle` module.

--- src/pocof.Benchmark/Program.fs
+++ src/pocof.Benchmark/Program.fs
@@ -3,7 +3,9 @@ open pocof.Benchmark
 
 [<EntryPoint>]
 let main argv =
-    BenchmarkSwitcher.FromTypes([| typeof<PocofBenchmarks>; typeof<KeysBenchmarks> |]).Run(argv)
+    BenchmarkSwitcher
+        .FromTypes([| typeof<PocofBenchmarks>; typeof<KeysBenchmarks>; typeof<HandleBenchmarks> |])
+        .Run(argv)
     |> ignore
 
     0 // return an integer exit code
