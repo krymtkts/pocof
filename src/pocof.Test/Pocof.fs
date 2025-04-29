36a9981fe3a743b9b321c243dffbcba7db88a0b3
krymtkts <takatoshi.kuriyama@gmail.com>
2025-04-29 16:25:59 +0900

    Refactor `renderOnce` tests to use Expecto instead of FsUnit.

--- src/pocof.Test/Pocof.fs
+++ src/pocof.Test/Pocof.fs
@@ -431,85 +431,88 @@ let tests_stopUpstreamCommandsException =
               |> Expect.equal "should return MockException type" typeof<MockException>
           } ]
 
-module renderOnce =
-    [<Tests>]
-    let tests =
-        testList
-            "renderOnce"
-            [ test "When handler is empty, should return ContinueProcessing.Continue" {
-                  let config: InternalConfig =
-                      { NotInteractive = false
-                        Layout = Layout.BottomUpHalf
-                        Keymaps = Keys.defaultKeymap
-                        WordDelimiters = ";:,.[]{}()/\\|!?^&*-=+'\"–—―"
-                        Prompt = prompt
-                        PromptLength = prompt |> String.length
-                        Properties = []
-                        PropertiesMap = Map [] }
+[<Tests>]
+let tests_renderOnce =
+    testList
+        "renderOnce"
+        [
 
-                  let handler = Pocof.RenderHandler()
-                  let rui = new MockRawUI()
-                  let buff = Screen.init (fun _ -> rui) (fun _ -> Seq.empty) config.Layout prompt
-                  let actual = Pocof.renderOnce handler buff
+          test "When handler is empty, should return ContinueProcessing.Continue" {
+              let config: InternalConfig =
+                  { NotInteractive = false
+                    Layout = Layout.BottomUpHalf
+                    Keymaps = Keys.defaultKeymap
+                    WordDelimiters = ";:,.[]{}()/\\|!?^&*-=+'\"–—―"
+                    Prompt = prompt
+                    PromptLength = prompt |> String.length
+                    Properties = []
+                    PropertiesMap = Map [] }
 
-                  actual
-                  |> function
-                      | Pocof.RenderProcess.Noop -> true
-                      | _ -> false
-                  |> Expect.equal "should return ContinueProcessing.Continue when handler is empty" true
-              }
-              test "When handler has a render event, should return ContinueProcessing.Continue" {
-                  let config: InternalConfig =
-                      { NotInteractive = false
-                        Layout = Layout.BottomUpHalf
-                        Keymaps = Keys.defaultKeymap
-                        WordDelimiters = ";:,.[]{}()/\\|!?^&*-=+'\"–—―"
-                        Prompt = prompt
-                        PromptLength = prompt |> String.length
-                        Properties = []
-                        PropertiesMap = Map [] }
+              let handler = Pocof.RenderHandler()
+              let rui = new MockRawUI()
+              let buff = Screen.init (fun _ -> rui) (fun _ -> Seq.empty) config.Layout prompt
+              let actual = Pocof.renderOnce handler buff
 
-                  let handler = Pocof.RenderHandler()
+              actual
+              |> function
+                  | Pocof.RenderProcess.Noop -> true
+                  | _ -> false
+              |> Expect.equal "should return ContinueProcessing.Continue when handler is empty" true
+          }
 
-                  (state, lazy PSeq.empty, lazy Error "error")
-                  |> Pocof.RenderEvent.Render
-                  |> handler.Publish
+          test "When handler has a render event, should return ContinueProcessing.Continue" {
+              let config: InternalConfig =
+                  { NotInteractive = false
+                    Layout = Layout.BottomUpHalf
+                    Keymaps = Keys.defaultKeymap
+                    WordDelimiters = ";:,.[]{}()/\\|!?^&*-=+'\"–—―"
+                    Prompt = prompt
+                    PromptLength = prompt |> String.length
+                    Properties = []
+                    PropertiesMap = Map [] }
 
-                  let rui = new MockRawUI()
-                  let buff = Screen.init (fun _ -> rui) (fun _ -> Seq.empty) config.Layout prompt
-                  let actual = Pocof.renderOnce handler buff
+              let handler = Pocof.RenderHandler()
 
-                  actual
-                  |> function
-                      | Pocof.RenderProcess.Rendered _ -> true
-                      | _ -> false
-                  |> Expect.equal "should return ContinueProcessing.Continue when handler has a render event" true
-              }
-              test "When handler has a quit event, should return ContinueProcessing.StopUpstreamCommands" {
-                  let config: InternalConfig =
-                      { NotInteractive = false
-                        Layout = Layout.BottomUpHalf
-                        Keymaps = Keys.defaultKeymap
-                        WordDelimiters = ";:,.[]{}()/\\|!?^&*-=+'\"–—―"
-                        Prompt = prompt
-                        PromptLength = prompt |> String.length
-                        Properties = []
-                        PropertiesMap = Map [] }
+              (state, lazy PSeq.empty, lazy Error "error")
+              |> Pocof.RenderEvent.Render
+              |> handler.Publish
 
-                  let handler = Pocof.RenderHandler()
-                  Pocof.RenderEvent.Quit |> handler.Publish
-                  let rui = new MockRawUI()
-                  let buff = Screen.init (fun _ -> rui) (fun _ -> Seq.empty) config.Layout prompt
-                  let actual = Pocof.renderOnce handler buff
+              let rui = new MockRawUI()
+              let buff = Screen.init (fun _ -> rui) (fun _ -> Seq.empty) config.Layout prompt
+              let actual = Pocof.renderOnce handler buff
 
-                  actual
-                  |> function
-                      | Pocof.RenderProcess.StopUpstreamCommands -> true
-                      | _ -> false
-                  |> Expect.equal
-                      "should return ContinueProcessing.StopUpstreamCommands when handler has a quit event"
-                      true
-              } ]
+              actual
+              |> function
+                  | Pocof.RenderProcess.Rendered _ -> true
+                  | _ -> false
+              |> Expect.equal "should return ContinueProcessing.Continue when handler has a render event" true
+          }
+
+          test "When handler has a quit event, should return ContinueProcessing.StopUpstreamCommands" {
+              let config: InternalConfig =
+                  { NotInteractive = false
+                    Layout = Layout.BottomUpHalf
+                    Keymaps = Keys.defaultKeymap
+                    WordDelimiters = ";:,.[]{}()/\\|!?^&*-=+'\"–—―"
+                    Prompt = prompt
+                    PromptLength = prompt |> String.length
+                    Properties = []
+                    PropertiesMap = Map [] }
+
+              let handler = Pocof.RenderHandler()
+              Pocof.RenderEvent.Quit |> handler.Publish
+              let rui = new MockRawUI()
+              let buff = Screen.init (fun _ -> rui) (fun _ -> Seq.empty) config.Layout prompt
+              let actual = Pocof.renderOnce handler buff
+
+              actual
+              |> function
+                  | Pocof.RenderProcess.StopUpstreamCommands -> true
+                  | _ -> false
+              |> Expect.equal "should return ContinueProcessing.StopUpstreamCommands when handler has a quit event" true
+          }
+
+          ]
 
 module Interval =
     open System.Threading
